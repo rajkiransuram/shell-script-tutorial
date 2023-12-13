bf27d7a22938c17ade0930ca2ecd23f3e26e0d93
rajkiransuram <suramrajkiran@gmail.com>
2023-12-13 23:14:41 +0800

    variable script commit

---
+++ variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Rajkiran
+PERSON2=Ibrahim
+PERSON3=Rajesh
+
+echo "$PERSON1 : Hello $PERSON3 Good Monring"
+echo "$PERSON2 :  hellow $PERSON1 Good Morning"
+echo "$PERSON3 : Hello $PERSON2 Good Morning"
