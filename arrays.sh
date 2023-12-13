094b2bf0ccdec3a6eaf2630268e07918b438dcf5
rajkiransuram <suramrajkiran@gmail.com>
2023-12-14 00:16:29 +0800

    add variable

--- arrays.sh
+++ arrays.sh
@@ -2,7 +2,7 @@
 
 FRUITS={"Apple","Banana","Carrot","Durian"}
 
-echo "First Value :${$FRUITS[0]}"
-echo "First Value :${$FRUITS[1]}"
-echo "First Value :${$FRUITS[2]}"
-echo "First Value :${$FRUITS[3]}"
+echo "First Value :${FRUITS[0]}"
+echo "First Value :${FRUITS[1]}"
+echo "First Value :${FRUITS[2]}"
+echo "First Value :${FRUITS[3]}"
