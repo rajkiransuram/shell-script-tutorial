b8d3e33a52fa384246f93f7bbd45128231c273e9
rajkiransuram <suramrajkiran@gmail.com>
2023-12-14 00:18:33 +0800

    add fruits

--- arrays.sh
+++ arrays.sh
@@ -1,8 +1,8 @@
 #!bin/bash
 
-FRUITS={"Apple","Banana","Carrot","Durian"}
+FRUITS=("Apple","Banana","Carrot","Durian") 
 
-echo "First Value :${FRUITS[0]}"
-echo "First Value :${FRUITS[1]}"
-echo "First Value :${FRUITS[2]}"
-echo "First Value :${FRUITS[3]}"
+echo "First Value : ${FRUITS[0]}"
+echo "First Value : ${FRUITS[1]}"
+echo "First Value : ${FRUITS[2]}"
+echo "First Value : ${FRUITS[3]}"
