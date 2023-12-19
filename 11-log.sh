2d12de035783e471a4f45981becc400545717f17
rajkiransuram <suramrajkiran@gmail.com>
2023-12-20 00:49:38 +0800

    shell script

--- 11-log.sh
+++ 11-log.sh
@@ -3,22 +3,25 @@
 ID=$(id -u)
 
 TIMESTAMP=$(date +%F-%H-%S)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
 
 LOGFILE="/tmp/$0-$TIMESTAMP.log"
 
 VALIDATE(){
     if [ $1 -ne 0 ]
     then
-        echo "ERROR:: Installing $2 is failed"
+        echo -e "ERROR:: $2 ... $R FAILED $N"
         exit 1
     else
-        echo "$2 ... is SUCCESS"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
 if [ $ID -ne 0 ]
 then
-    echo "ERROR:: Plaase ru this scrpt with root user"
+    echo -e "$R ERROR:: Please run this script with root access $N"
     exit 1 # you can give other than 0 
  else
     echo "You are root user"
@@ -30,4 +33,4 @@ then
 
   yum install git -y &>> $LOGFILE
 
-  VALIDATE $? "Installing Git"
+  VALIDATE $? "Installing Git"  
