5031218b767a86d14e94dd97988b0a682f65bdab
rajkiransuram <suramrajkiran@gmail.com>
2023-12-20 00:17:51 +0800

    shell script

---
+++ 06-dataTypes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+NUMBER1=$1
+NUMBER2=$2
+
+SUM=$(($NUMBER1+$NUMBER2));
+
+echo "Total :: $SUM"
+
+echo "How many arguments passed :: $#"
+
+echo "All arguments passed :: $@"
+
+echo "Shell script name :: $0"
