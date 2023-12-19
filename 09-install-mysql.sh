d66cb834f2c6df196622a43a57a8ba4d4685cb8a
rajkiransuram <suramrajkiran@gmail.com>
2023-12-19 22:45:33 +0800

    shell script

--- 09-install-mysql.sh
+++ 09-install-mysql.sh
@@ -2,7 +2,7 @@
 
 ID=$(id -u)
 
-if[ $ID -ne 0 ]
+if [ $ID -ne 0 ]
 then
     echo "ERROR:: Plaase ru this scrpt with root user"
     exit 1 # you can give other than 0 
@@ -12,10 +12,10 @@ then
 
  yum install mysql -y
 
- if [$? -ne 0]
+ if [ $? -ne 0 ]
  then
     echo "ERROR:: Installing MySQL is failed"
     exit 1
  else
     echo "Installing MySQL SUCCESS"
- fi            
+ fi       
